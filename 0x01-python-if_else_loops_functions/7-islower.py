5aae4c3b8d9b504e812219289713aba79967bafc
Adeogun Michael <softworld17@gmail.com>
2022-12-07 15:59:29 -0800

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
+#!/urs/bin/python3
 def islower(c):
-    for i in range(ord('a'), ord('z')):
-        for n in range(ord('A'), ord('Z')):
-            if (i <= c) and (c >= c):
-                return True
-                if (n <= c) and (n >= c):
-                    return False
+    if ord(c) <= 97 and ord(c) <= 122:
+        return True
+    else:
+        return
