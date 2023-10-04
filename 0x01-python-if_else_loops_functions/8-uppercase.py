72249f3fa88adeef9f52a06a4ad218cd711749e5
Adeogun Michael <softworld17@gmail.com>
2023-10-04 09:53:38 -0700

    task

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for k in str:
+        if ord(k) >= 97 and ord(k) <= 122:
+            i = chr(ord(k) - 32)
+            print("{}" .format(k), end='')
+            print()
